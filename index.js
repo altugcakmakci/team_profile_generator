9fb4f5e25bd3cb111a2c0b03c6ea5f0ce73f8743
Altug Cakmakci <altugcakmakci@gmail.com>
2021-10-11 08:38:15 -0400

    functionallty added

--- index.js
+++ index.js
@@ -1,105 +1,101 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+function Teammember(mname, mid, memail, mdet, mtype) {
+    this.mname = mname;
+    this.mid = mid;
+    this.memail = memail;
+    this.mdet = mdet;
+    this.mtype = mtype;
+}
+
+Teammember.prototype.getHtmlBlock = function () {
+    let detail = "Office number";
+    if (this.mtype !== "Team manager") {
+        detail = "GitHub";
+    }
+    let icontxt = "<i class='fas fa-users'></i>";
+    if (this.mtype === "Engineer") {
+        icontxt = "<i class='fas fa-user-clock'></i>";
+    } else if (this.mtype === "Intern") {
+        icontxt = "<i class='fas fa-user-edit'></i>";
+    } 
+    let htmlBlock = `<div class="col-12 col-md-3 col-xl-3 box_item">
+    <div class='box_header'>
+    <p class="body_text">${this.mname}</p>
+    <p class="body_text">${icontxt} ${this.mtype}</p>
+    </div>
+    <p class="body_text">Id: ${this.mid}</p>
+    <p class="body_text">Email: ${this.memail}</p>
+    <p class="body_text">${detail}: ${this.mdet}</p>
+    </div>`;
+    return htmlBlock;
+};
+
+let teammembers = [];
+
 const questions = [
     {
         type: 'input',
-        message: 'What is your name?',
-        name: 'username',
+        message: 'What is the team manager’s name?',
+        name: 'membername',
     },
     {
         type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
+        message: 'What is the team manager’s id?',
+        name: 'memberid',
     },
     {
         type: 'input',
-        message: 'Where are you from?',
-        name: 'place',
+        message: 'What is the team manager’s email?',
+        name: 'memberemail',
     },
     {
         type: 'input',
-        message: 'Please write a short bio: ',
-        name: 'shortbio',
-    },
-    {
-        type: 'checkbox',
-        name: 'technologies',
-        message: 'Plese select the technologies you are familar with:',
-        choices: ['HTML', 'CSS', 'Javascript','JQuery','Node',"Express"]
-    },
-    {
-        type: 'input',
-        message: 'What is your linked in URL?',
-        name: 'linkedin'
-    },
-    {
-        type: 'input',
-        message: 'What is your Github URL?',
-        name: 'github'
+        message: 'What is the team manager’s office number?',
+        name: 'memberdet',
     },
     {
         type: 'list',
-        name: 'hbackcolor',
-        message: 'Plese choose a background color for header:',
-        choices: ['black', 'darkblue', 'brown','darkgreen','yellow',"cyan"],
-        default: 'darkblue'
-    },
-    {
-        type: 'list',
-        name: 'hfontcolor',
-        message: 'Plese choose a font color for header:',
-        choices: ['white', 'yellow', 'cyan','lightgreen','blue',"black"],
-        default: 'white'
-    },
-    {
-        type: 'list',
-        name: 'bbackcolor',
-        message: 'Plese choose a background color for body:',
-        choices: ['black', 'darkblue', 'brown','darkgreen','yellow',"cyan","white"],
-        default: 'darkblue'
-    },
-    {
-        type: 'list',
-        name: 'bfontcolor',
-        message: 'Plese choose a font color for body:',
-        choices: ['white', 'yellow', 'cyan','lightgreen','blue',"darkblue","black"],
-        default: 'white'
+        name: 'membertype',
+        message: 'Which type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'I am done'],
+        default: 'Engineer'
     }
 ];
 
-// Create a function to write README file
-function writeHtmlFile(filename, data) {
-    fs.writeFile(`./${filename}`, generateHTML(data), (err) =>
+function writeHtmlFile(filename) {
+    fs.writeFile(`./${filename}`, generateHTML(), (err) =>
         err ? console.error(err) : console.log('HTML file created!')
     );
 }
 
-function writeStyleToFile(filename, data) {
+function writeStyleToFile(filename) {
     if (!fs.existsSync('./css')) {
         fs.mkdirSync('./css');
     }
-    fs.writeFile(`./css/${filename}`, generateCSS(data), (err) =>
+    fs.writeFile(`./css/${filename}`, generateCSS(), (err) =>
         err ? console.error(err) : console.log('CSS file created!')
     );
 }
 
-function produceDocs(data){
-    writeStyleToFile("style.css",data);
-    writeHtmlFile("index.html",data);
+function produceDocs() {
+    console.log("Produce docs");
+    writeStyleToFile("style.css");
+    writeHtmlFile("index.html");
 }
 
-// Create a function to initialize app
-function init() {
-    inquirer
-        .prompt(questions)
-        .then((response) =>
-            produceDocs(response)
-        );
+function addTeamMember(type, answer) {
+    let newmember = new Teammember(answer.membername, answer.memberid, answer.memberemail, answer.memberdet, type);
+    teammembers.push(newmember);
+    if (answer.membertype === "I am done") {
+        produceDocs();
+    }
 }
 
-function generateHTML(data) {
-    let retStr= `<!DOCTYPE html>
+
+function generateHTML() {
+    let retStr = `<!DOCTYPE html>
     <html lang="en">
     
     <head>
@@ -109,80 +105,90 @@ function generateHTML(data) {
       <meta name="Description" content="Enter your description here" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
       <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed&display=swap" rel="stylesheet">
+      <script src="https://kit.fontawesome.com/525618399c.js" crossorigin="anonymous"></script>      <link rel="stylesheet" type="text/css" href="./css/style.css" />
       <link rel="stylesheet" type="text/css" href="./css/style.css" />
-      <title>Personal Portfolio</title>
+      <title>My Team</title>
     </head>
     
     <body>
-    <h1>My personal portfolio!</h1>
-    <p class="body_text text-center">My name is ${data.username}, and I am from ${data.place}.</p>
-
+    <h1>My Team</h1>
+ 
     <section class="container my-5">
-    <div class="row">
-      <div class="col-12 col-md-5 col-xl-5 box_item">
-        <p class="body_text text-center">${data.shortbio}</p>
-        </div>
-        <div class="col-12 col-md-6 col-xl-6 box_item">
-    <h2>Here is a list of technologies that I am familiar with<h2>
-    <ul>`;
-    console.log(data);
+    <div class="row justify-content-around">
+   `;
 
-    for (const i of data.technologies) {
-        retStr = retStr+`<li class="list-group-item">${i}</li>`;
+    for (const teammember of teammembers) {
+        retStr = retStr + teammember.getHtmlBlock();
     }
 
-    retStr = retStr+`
-    </ul>
+    retStr = retStr + `
     </div>
-    </div>
-    <hr>
-    <a href="${data.github}">You can view my work in Github</a>
-    <hr>
-
-    <div class="footer">
-    <h2>Contact</h2>
-    <p class="body_text">Email: ${data.email}</p>
-    <p class="body_text">Linkedin: ${data.linkedin}</p>
-    </div>
-    </body>
+  </body>
 
     </html>    
   `;
     return retStr;
-  }
+}
 
-  function generateCSS (data){
-      return `h1 {
-        background-color: ${data.hbackcolor};
-        color: ${data.hfontcolor};
-        text-align: center;
-        padding:10px;
-        font-family: 'Barlow Semi Condensed', sans-serif;
-    }
-    body {
-        background-color: ${data.bbackcolor};
-        color: ${data.bfontcolor};
-        font-family: 'Barlow Semi Condensed', sans-serif;
-    }
+function generateCSS() {
+    return `h1 {
+    background-color: red;
+    color: white;
+    text-align: center;
+    padding:10px;
+    font-family: 'Barlow Semi Condensed', sans-serif;
+}
+body {
+    background-color: white;
+    color: black;
+    font-family: 'Barlow Semi Condensed', sans-serif;
+}
     
-    .body_text {
-        font-size: 24px;
-    }
-    .box_item {  
-        border: solid 2px ${data.bfontcolor}; 
-        border-radius: 5px;
-        padding: 10px;
-        margin: 10px;
-    }
-    .footer {
-        background-color: ${data.hbackcolor};
-        color: ${data.hfontcolor};
-        text-align: center;
-        padding:10px;
-        font-family: 'Barlow Semi Condensed', sans-serif;
-    }
-    `;
-  }
+.body_text {
+    font-size: 24px;
+}
 
-// Function call to initialize app
-init();
+.box_header{
+    background-color: blue;
+    padding-left: 5px;
+    color: white;
+}
+
+.box_item {  
+    border: solid 2px blue; 
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px;
+}
+`;
+}
+
+
+function getAnswers(type) {
+    console.log("Here3");
+    return inquirer.prompt(questions).then((answers) => {
+        console.log(answers.membertype);
+        if (answers.membertype === "I am done") {
+            console.log("Here5");
+            addTeamMember(type, answers);
+            return answers;
+        } else {
+            console.log("Here");
+            addTeamMember(type, answers);
+            questions[0].message = `What is the ${answers.membertype}'s name?`
+            questions[1].message = `What is the ${answers.membertype}'s id?`
+            questions[2].message = `What is the ${answers.membertype}'s email?`
+            questions[3].message = `What is the ${answers.membertype}'s GitHub username?`
+            console.log("Here2");
+            return getAnswers(answers.membertype);
+        }
+    });
+}
+
+function main() {
+    getAnswers("Team manager")
+        .then(console.log)
+        .catch((error) => { });
+}
+
+main();
